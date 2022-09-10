8a6cd85959844305d54f0ea932951f7670f7d55e
BackFox <jorealbusiness@gmail.com>
2022-09-10 14:32:09 +0900

    #7 - RefactoringTodoService의 RefactoringTodo 저장 로직 tdd를 이용해 구현하기
    리팩토링 대상 코드를 저장하는 비즈니스 로직을 tdd 방식으로 테스트하고 구현했습니다.
    
    엔티티 객체를 컨트롤러 계층에서 조작하는 것을 방지하기 위해, 컨트롤러 계층에서는 필요한 정보가 들어간 RefactoringTodoOrderFormat과 RefactoringTodoServiceTest라는 Dto를 서비스 계층에 전달하고, 비즈니스 로직 내에서 Dto를 엔티티 객체로 바꾼 후 리포지토리 계층에 전달하도록 구현하였습니다.
    
    이 과정에서 엔티티 클래스에도 수정이 다소 일어나서,
    RefactoringTodoOrder에 대해 OnetoMany 연관관계를 추가해 양방향 연관관계로 만든 후, RefactoringTodo 저장 시 함께 저장될 수 있도록 CascadeType.PERSIST 옵션을 설정했습니다.
    
    엔티티의 값을 바꾸는 도메인 로직의 경우에는 단순히 setter문을 사용하지 않고, assignRefactoringTodo, addRefactoringOrder처럼 의도를 비교적 명확히 하는 메서드명과 함께 의도에 적합한 검증 로직을 추가로 넣어두었습니다.

--- src/main/java/com/refactoring/refactoringproject/entity/RefactoringTodoOrder.java
+++ src/main/java/com/refactoring/refactoringproject/entity/RefactoringTodoOrder.java
@@ -1,9 +1,21 @@
 package com.refactoring.refactoringproject.entity;
 
+import lombok.Getter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
 public class RefactoringTodoOrder extends BaseEntityTime {
+    public RefactoringTodoOrder() {
+    }
+
+    public RefactoringTodoOrder(Long id, RefactoringTodo refactoringTodo, String content) {
+        this.id = id;
+        this.refactoringTodo = refactoringTodo;
+        this.content = content;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -14,4 +26,12 @@ public class RefactoringTodoOrder extends BaseEntityTime {
 
     @Column(length = 200, nullable = false)
     private String content;
+
+    public void assignRefactoringTodo(RefactoringTodo refactoringTodo) {
+        if (this.refactoringTodo != null) {
+            throw new IllegalArgumentException("You can't change RefactoringTodo of RefactoringOrder which is already assigned.");
+        }
+
+        this.refactoringTodo = refactoringTodo;
+    }
 }
