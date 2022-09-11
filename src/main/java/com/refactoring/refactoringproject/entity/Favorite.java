2ec8d009686dc003781e827eb25ebe3408225864
BackFox <jorealbusiness@gmail.com>
2022-09-11 19:04:01 +0900

    #20 - MemberService의 리팩토링 대상 코드 즐겨찾기 등록 로직 tdd로 구현하기
    Favorite 엔티티 객체이므로 FavoriteService를 따로 만들어야 하나 고민했는데, 회원 정보와 연관된 정보이고 프로젝트 특성상 Member를 조회하면서 함께 조회할 일이 많기 때문에 MemberService의 비즈니스 로직에 포함하기로 했습니다.
    
    Favorite를 저장할 때 제공해야 하는 정보는 즐겨찾기를 진행하는 사용자 정보와 즐겨찾기 대상인 리팩토링 대상 코드인데, 사용자 정보는 스프링 시큐리티 적용 후에는 SecurityContextHolder에서 가져와 조회할 것이기 때문에 컨트롤러에서 할 일이 없고, 리팩토링 대상 코드의 경우 아이디만 넘겨주면 비즈니스 로직에서 객체를 찾아 작업을 이어나갑니다. 따라서 Favorite과 관련된 dto는 필요하지 않습니다.
    
    favorite 엔티티를 저장하는 로직 실행 후에도 트랜잭션이 끝나지 않으면 flush를 호출하지 않기 때문에, 트랜잭션이 끝나기 전에 member.getFavorite()를 호출하면 제대로 반영이 안 되어 있습니다. 따라서 순수 자바 로직에서도 잘 조회가 되도록 비즈니스 로직 내에서 addFavorite()를 호출해 member.favorites에 favorite를 수동으로 할당해 주었습니다.

--- src/main/java/com/refactoring/refactoringproject/entity/Favorite.java
+++ src/main/java/com/refactoring/refactoringproject/entity/Favorite.java
@@ -1,18 +1,34 @@
 package com.refactoring.refactoringproject.entity;
 
+import lombok.Getter;
+import org.springframework.data.util.Lazy;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
 public class Favorite extends BaseEntityTime {
+    public Favorite() {
+    }
+
+    public Favorite(Member member, RefactoringTodo refactoringTodo) {
+        this.member = member;
+        this.refactoringTodo = refactoringTodo;
+    }
+
+    public static Favorite of(Member member, RefactoringTodo refactoringTodo) {
+        return new Favorite(member, refactoringTodo);
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID", nullable = false)
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "REFACTORING_TODO_ID", nullable = false)
     private RefactoringTodo refactoringTodo;
 }
--- src/main/java/com/refactoring/refactoringproject/service/MemberService.java
+++ src/main/java/com/refactoring/refactoringproject/service/MemberService.java
@@ -1,25 +1,44 @@
 package com.refactoring.refactoringproject.service;
 
 import com.refactoring.refactoringproject.dto.MemberSignInFormat;
+import com.refactoring.refactoringproject.entity.Favorite;
 import com.refactoring.refactoringproject.entity.Member;
+import com.refactoring.refactoringproject.entity.RefactoringTodo;
+import com.refactoring.refactoringproject.repository.FavoriteRepository;
 import com.refactoring.refactoringproject.repository.MemberRepository;
+import com.refactoring.refactoringproject.repository.RefactoringTodoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @Slf4j
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final RefactoringTodoRepository refactoringTodoRepository;
+    private final FavoriteRepository favoriteRepository;
 
     public void signIn(MemberSignInFormat signInFormat) {
         if (memberRepository.findById(signInFormat.getEmail()).isPresent()) {
             throw new IllegalArgumentException("Member already exists with this email: " + signInFormat.getEmail());
-        }
+        } // TODO: insert문에서 어차피 id 중복여부를 확인할 텐데 select문을 굳이 쏴 id 중복 여부를 확인하고 있다.
 
         Member member = MemberSignInFormat.toEntity(signInFormat);
         Member savedMember = memberRepository.save(member);
         log.info("Signing In Completed. Member ID: {}", savedMember.getId());
+    }
+
+    public void assignFavorite(Long refactoringTodoId, Member member) { //TODO: 스프링 시큐리티 도입 이후에는 파라미터로 꺼낼 필요가 없다.
+        RefactoringTodo refactoringTodo = refactoringTodoRepository.findById(refactoringTodoId)
+                .orElseThrow(() -> new EmptyResultDataAccessException("there is no RefactoringTodo with id " + refactoringTodoId, 1));
+
+        Favorite favorite = Favorite.of(member, refactoringTodo);
+        favoriteRepository.save(favorite);
+
+        member.addFavorite(favorite);
     }
 }
